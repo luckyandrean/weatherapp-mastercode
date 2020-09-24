9f8be1eb3bd9ae74486d36699f0edd591560b63f
luckyandrean <71387629+luckyandrean@users.noreply.github.com>
2020-09-24 09:17:34 +0700

    Add files via upload

---
+++ build/precache-manifest.6f03270ddcbc9c52c6a13b59964cc7f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "116d7c043b5927f903f8ffda3e0a69c4",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "efd9fdc7f1cb43dd6c1a",
+    "url": "/weather/static/css/main.b3206743.chunk.css"
+  },
+  {
+    "revision": "7cf2182656a62ed6117a",
+    "url": "/weather/static/js/2.d437b892.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather/static/js/2.d437b892.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efd9fdc7f1cb43dd6c1a",
+    "url": "/weather/static/js/main.b0fa745d.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "96834be1098799a45c98d95c2041e54f",
+    "url": "/weather/static/media/cold.96834be1.jpeg"
+  },
+  {
+    "revision": "8533d25f75b7497d151971629b0f102f",
+    "url": "/weather/static/media/warm.8533d25f.jpg"
+  }
+]);
